ce43876dff8185b9ea33f20a65491ae3dcb6b08a
cmstexas <crystal.m.soria@gmail.com>
2019-07-25 12:03:20 -0500

    mvp project from solution

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,62 +1,84 @@
-import React, { useState} from 'react';
-// import { BrowserRouter as Router, Route} from "react-router-dom";
-// import './App.css';
-import TeamData from './TeamData';
-import Form from './Form';
 
+import React, { useState } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import Member from "./Member";
+import Form from "./Form";
 
-const Teammembers = () => {
-  const [team, setTeam] = useState(TeamData);
-  console.log(team)
+function App() {
+  const [members, setMembers] = useState([]);
 
-  const formChange = newMember => {
-    setTeam([...team, newMember]);
-  };
+  const submitMember = member => setMembers([...members, member]);
+  const update = uMember =>
+    setMembers([
+      ...members.map(member => {
+        if (member.id === uMember.id) {
+          return uMember;
+        }
+        return member;
+      })
+    ]);
 
-  // const team = response.data.results;
-  // setTeam(team);
+  console.log("members", members);
 
   return (
     <div className = "member-list">
-      <h2> Sign Up to Join the Team </h2>
-      <Form teamAdd={formChange} />
+       <h1> Sign Up to Join the Team </h1>
+     <Form add={submitMember} />
       <h1>Our Current Team Members</h1>
-    <div>
-     {team.map((member, index) => {
-       return (
-        <p>
-          {team.name}
-          {team.email}
-          {team.role}
-          {team.skill}
-        </p>
-       )
-     })}
+      {members.map((member, index) => (
+       <Member key={index} member={member} update={update} />
+     ))}
     </div>
-  </div>
-    );
+  );
 }
 
-export default Teammembers;
+export default App;
 
 
 
 
+
+
+
+// OLD CODE
 // import React, { useState} from 'react';
-// import { BrowserRouter as Router, Route} from "react-router-dom";
+// // import { BrowserRouter as Router, Route} from "react-router-dom";
 // import './App.css';
-// import TeamData from './TeamData';
+// import Member from "./components/Member";
+// import Form from './Form';
 
 
-// export default function App() {
-//   const [name, setName] = useState( [] );
+// function App() {
+//   const [members, setMembers] = useState([]);
+
+//   const submitMember = member => setMembers([...members, member]);
+//     const update = uMember =>
+//       setMembers([
+//         ...members.map(member => {
+//           if (member.id === uMember.id) {
+//             return uMember;
+//           }
+//           return member;
+//         })
+//       ])
+//   };
+
+//   console.log("members", members);
+
 
 //   return (
-//   <Router>
-//    <div>
-//      <TeamData list={TeamData} />
-//        <Route path='/' exact component={TeamData} />
-//    </div>
-//  </Router>
-//   );
-// }
+//     <div className = "member-list">
+//       <h2> Sign Up to Join the Team </h2>
+//       <Form add={submitMember} />
+//       <h1>Our Current Team Members</h1>
+//      {members.map((member, index) => (
+//        <Member key={index} member={member} update={update} />
+//      ))}
+//     </div>
+//     );
+
+// export default App;
+
+
+
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,64 +1,167 @@
-import React, { useState } from 'react';
 
-function Form(props) {
+import React, { useState } from "react";
 
-    const [teamMate, setState] = useState ({
-        Name: "",
-        Email: "",
-        Role: "",
-        Skill: "",
+const Member = props => {
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    role: "",
+    id: null
+  });
+
+  const inputHandler = e => {
+    console.log("target name", e.target.name);
+    console.log("value", e.target.value);
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+    props.add({ ...input, id: Math.random() });
+    setInput({
+      name: "",
+      email: "",
+      role: "",
+      id: null
     });
+  };
+  console.log("props", props);
+  console.log("input", input);
+  return (
+    <form onSubmit={submitHandler}>
+      <h1>Add Member</h1>
+      <div>
+        <label htmlFor="Name">
+          Name:{" "}
+          <input
+            type="text"
+            value={input.name}
+            onChange={inputHandler}
+            name="name"
+          />
+        </label>
+        <label htmlFor="Email">
+          Email:{" "}
+          <input
+            type="text"
+            value={input.email}
+            onChange={inputHandler}
+            name="email"
+          />
+        </label>
+        <label htmlFor="Role">
+          Role:{" "}
+          <input
+            type="text"
+            value={input.role}
+            onChange={inputHandler}
+            name="role"
+          />
+        </label>
+      </div>
+      <button className="formbutton">Add Member</button>
+    </form>
+  );
+};
 
-    setState({
-        Name: "",
-        Email: "",
-        Role: "",
-        Skill: "",
-    })
-
-
-    const submitHandler = (event) => {
-        event.preventDefault();
-        console.log("teamMate", teamMate);
-    };
-
-    return (
-        <div>
-            <form onsubmit={submitHandler}>
-                <div>
-                    <label> Team Sign up Form</label>
-                    <input>
-                        type="text"
-                        name="name"
-                        placeholder="enter name here"
-                        value={teamMate.name}
-                    </input>
-                    <input>
-                        type="email"
-                        name="email"
-                        placeholder="enter email here"
-                        value={teamMate.email}
-                    </input>
-                    <input>
-                        type="text"
-                        name="role"
-                        placeholder="enter role here"
-                        value={teamMate.role}
-                    </input>
-                    <input>
-                        type="text"
-                        name="skill"
-                        placeholder="enter skill here"
-                        value={teamMate.skill}
-                    </input>
-                </div>
-                <button type="submit">Submit Now</button>
-            </form>
-        </div>
-    )
-}
-
-export default Form;
+export default Member;
 
 
-{/* fieldset and grouping: https://www.w3schools.com/tags/tag_fieldset.asp */}
+
+
+// OLD CODE
+// import React, { useState } from 'react';
+
+// const Member = props => {
+//     const [input, setInput] = useState ({
+//         name: "",
+//         email: "",
+//         role: "",
+//         skill: "",
+//         id: null
+//     });
+
+//     // setState({
+//     //     Name: "",
+//     //     Email: "",
+//     //     Role: "",
+//     //     Skill: "",
+//     // })
+
+//     const inputHandler = event => {
+//         console.log("target name", event.target.name);
+//         console.log("value", event.target.value);
+//         setInput({ ...input, [event.target.name]: event.target.value });
+//     };
+
+
+//     const submitHandler = (event) => {
+//         event.preventDefault();
+//         props.add({ ...input, id: Math.random() });
+//         setInput({
+//             name: "",
+//             email: "",
+//             role: "",
+//             skill: "",
+//             id: null
+//         });
+//     };
+//     console.log("props", props);
+//     console.log("input", input);
+
+//     return (
+//         <div>
+//             <form onSubmit={submitHandler}>
+//                 <div>
+//                     <h1> Team Sign up Form</h1>
+//                     <label htmlFor="Name">
+//                     Name:{" "}
+//                     <input>
+//                         type="text"
+//                         name="name"
+//                         placeholder="enter name here"
+//                         value={input.name}
+//                         onChange={inputHandler}
+//                     </input>
+//                     </label>
+//                     <label htmlFor="Email">
+//                     Email:{" "}
+//                     <input>
+//                         type="email"
+//                         name="email"
+//                         placeholder="enter email here"
+//                         value={input.email}
+//                         onChange={inputHandler}
+//                     </input>
+//                     </label>
+//                     <label htmlFor="Role">
+//                     Role:{" "}
+//                     <input>
+//                         type="text"
+//                         name="role"
+//                         placeholder="enter role here"
+//                         value={input.role}
+//                         onChange={inputHandler}
+//                     </input>
+//                     </label>
+//                     <label htmlFor="Skill">
+//                     Skill:{" "}
+//                     <input>
+//                         type="text"
+//                         name="skill"
+//                         placeholder="enter skill here"
+//                         value={input.skill}
+//                         onChange={inputHandler}
+//                     </input>
+//                     </label>
+//                 </div>
+//                 <button type="submit">Add New Member</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default Member;
+
+
+// {/* fieldset and grouping: https://www.w3schools.com/tags/tag_fieldset.asp */}
